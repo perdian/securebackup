0dbc25617b83b1f84f8ae7f0113a3a6a5e3197c7
Christian Seifert <dev@perdian.de>
2023-08-13 22:03:59 +0200

    Initial release

---
+++ src/main/java/de/perdian/apps/securebackup/fx/SecureBackupApplicationLauncher.java
@@ -0,0 +1,9 @@
+package de.perdian.apps.securebackup.fx;
+
+public class SecureBackupApplicationLauncher {
+
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+
+}
